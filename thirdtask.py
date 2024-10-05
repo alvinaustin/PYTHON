f69002a87b8d1b4e5ee2a53375005ecc90c3f521
alvinaustin <alvinjosephaustin2028@cs.sjcetpalai.ac.in>
2024-10-05 15:08:55 +0530

    Add files via upload
    third task

---
+++ thirdtask.py
@@ -0,0 +1,21 @@
+#author:alvin austin
+#date:05-10-24
+#program that demonstrates the usage of arithmetic, comparison, and logical operators. Perform a few operations and print the results.
+num1=10
+num2=5
+summation=num1+num2
+division=10/5
+print(summation)
+print(division)
+if num1>num2:
+    print("is num1 greater than num2? true!")
+else:
+    print("false")
+
+if num1==num2:
+    print("is num1=num2? true")
+else:
+    print("is num1=num2?false!")
+
+print('logical AND:',num1>num2)
+print("logical OR:",num1>num2)
