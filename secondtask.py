55833a0111c98df8d25369e36c1163a105a709e1
alvinaustin <alvinjosephaustin2028@cs.sjcetpalai.ac.in>
2024-10-05 15:08:31 +0530

    Add files via upload
    second task

---
+++ secondtask.py
@@ -0,0 +1,6 @@
+#author:alvin austin
+#date:05-10-24
+#Python program that stores a string in a variable. Extract a specific part of the string (substring) and then concatenate it with another string. Finally, display the new string.
+name="hello world"
+extractor=name[5:11]
+print("everyone"+extractor)
