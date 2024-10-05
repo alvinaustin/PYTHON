c34c5d17f793a9d8b3414bebe641c48aa38b9e18
alvinaustin <alvinjosephaustin2028@cs.sjcetpalai.ac.in>
2024-10-05 15:06:41 +0530

    first task

---
+++ firsttask.py
@@ -0,0 +1,6 @@
+#author:alvin austin
+#date:05-10-24
+#program that asks the user to input their name and age. After receiving the input, display a message that includes the user's name and age.
+name=input("Enter your name:")
+age=int(input("Enter your age"))
+print("hello",name,"! your age is",age)
