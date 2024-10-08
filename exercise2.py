ce5d8a25d0284f98077cfcdd64d4a88f4f7780ea
alvinaustin <alvinjosephaustin2028@cs.sjcetpalai.ac.in>
2024-10-08 14:43:00 +0530

    Add files via upload
    exercise2

---
+++ exercise2.py
@@ -0,0 +1,12 @@
+#author:Alvin Austin
+#date:8-10-24
+first_name="Alvin"
+second_name="Austin"
+combiner=first_name+" "+second_name
+extractor=combiner[6:12]
+print(combiner)
+print("the extracted substring is:",extractor)
+
+
+
+
