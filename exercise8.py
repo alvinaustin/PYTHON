ed4b3c5ebe069d72eb7add0aaca1ad773f5810ba
alvinaustin <alvinjosephaustin2028@cs.sjcetpalai.ac.in>
2024-10-15 15:11:26 +0530

    exercise8

---
+++ exercise8.py
@@ -0,0 +1,8 @@
+#author=Alvin Austin
+#date=15-10-24
+num1=int(input("Enter a Number"))
+num2=int(input("Enter a Number"))
+if num1>num2:
+    print("NUM1 ias gretarer than NUM2")
+else:
+    print("NUM2 is greater than NUM1")
