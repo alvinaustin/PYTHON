93d96618edfa88fa3abfce905fe51b7d8309be42
alvinaustin <alvinjosephaustin2028@cs.sjcetpalai.ac.in>
2024-10-15 15:18:47 +0530

    Add files via upload
    exercise10

---
+++ exercise10.py
@@ -0,0 +1,9 @@
+#author=Alvin Austin
+#date=15-10-24
+age=int(input("Enter the age:"))
+if age<10 :
+    print("Ticket fare is 7 ")
+elif age>=10:
+    print("Ticket fare is 10")
+else:
+    print("the ticket fare is 5")
