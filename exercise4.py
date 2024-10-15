2448c819a77215d49627c8e0bcec8f84e8e1c4a5
alvinaustin <alvinjosephaustin2028@cs.sjcetpalai.ac.in>
2024-10-15 14:03:52 +0530

    Add files via upload
    exercise4

---
+++ exercise4.py
@@ -0,0 +1,10 @@
+#author=Alvin Austin
+#date=15-10-24
+#program to check whether the number is negative or positive
+num=int(input("Enter number:" ))
+if num>0:
+    print("Number",num,"is positive")
+elif num<0:
+    print("Number",num,"is negative")
+else:
+    print("the number is 0")
