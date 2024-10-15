c7746778660c91fdceae4b46f6e4a00be104db2a
alvinaustin <alvinjosephaustin2028@cs.sjcetpalai.ac.in>
2024-10-15 15:35:11 +0530

    Add files via upload
    exercise11

---
+++ exercise11.py
@@ -0,0 +1,7 @@
+#author=Alvin Austin
+#date=15-10-24
+pr_amount=float(input("Enter principle amount:"))
+time=int(input("Enter time"))
+rate=float(input("Enter rate"))
+si=pr_amount*time*rate
+print(si)
